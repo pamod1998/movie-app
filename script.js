e724433755395d6ad76e8c420270a4b701073e0c
pamod1998 <pamod145@gmail.com>
2022-01-27 17:29:43 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const APIKEY =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=475438d5cd8429c0d047690c4510b8da&page=1";
+const IMGPATH = "https://api.themoviedb.org/t/p/w1280";
+const SEARCHAPI =
+  "https://api.themoviedb.org/3/search/movie?&api_key=475438d5cd8429c0d047690c4510b8da&query=";
